4dfa2fb5099f4e291e141501c5789b797f415460
Ng-Pham <nghipham2003@gmail.com>
2024-05-14 00:12:09 +0700

    them form

---
+++ Eco_Bicycle_for_Rent.Bussiness/DSTramTaiChiNhanhBUS.cs
@@ -0,0 +1,27 @@
+﻿using Eco_Bicycle_for_Rent.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eco_Bicycle_for_Rent.Bussiness
+{
+    public class DSTramTaiChiNhanhBUS
+    {
+        DSTramTaiChiNhanhDL dst=new DSTramTaiChiNhanhDL();
+        public DataTable DSTramTaiCN()
+        {
+          return dst.LayDSTramTaiCN();
+        }
+        public DataTable DSCN()
+        {
+            return dst.LayDSCN();
+        }
+        public DataTable LayDSTramTheoCN(int macn)
+        {
+            return dst.LayDSTramTheoCN(macn);
+        }
+    }
+}
